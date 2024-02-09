b79589ec2c50518b3c5c84c5dc8440b5c83efedd
sFaucher1020 <fauchersamuel02@gmail.com>
2024-02-09 01:49:17 -0500

    added edit pin section and hooked up swiftdata, pins now stay in database when app is closed and reopened, users can now view pin description and delete pins willingly

---
+++ senProjTest/pinListView.swift
@@ -0,0 +1,18 @@
+//
+//  pinListView.swift
+//  senProjTest
+//
+//  Created by Samuel Faucher on 2/8/24.
+//
+
+import SwiftUI
+
+struct pinListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    pinListView()
+}
